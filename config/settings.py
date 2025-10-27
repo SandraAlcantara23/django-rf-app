7ad4ae791e88c40b900a44299a94ff2a7151ef23
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2025-10-26 19:56:35 -0600

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -55,12 +55,12 @@ TEMPLATES = [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.text",
                 "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = "config.wsgi.application"
 
